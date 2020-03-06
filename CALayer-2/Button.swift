2aaa00e321fbf1e1baf033a9936293654b161719
Sergei Demianenko <siriysgrey@gmail.com>
2020-03-06 12:00:41 +0200

    first commit

---
+++ CALayer-2/Button.swift
@@ -0,0 +1,21 @@
+//
+//  Button.swift
+//  CALayer-2
+//
+//  Created by Serhii Demianenko on 06.03.2020.
+//  Copyright © 2020 Serhii Demianenko. All rights reserved.
+//
+
+import UIKit
+
+class Button: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
