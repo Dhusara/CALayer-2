d9a24e1af640052cb5747aa9d488310e9cedc0b6
Sergei Demianenko <siriysgrey@gmail.com>
2020-03-06 10:56:05 +0200

    Initial Commit

---
+++ CALayer-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CALayer-2
+//
+//  Created by Serhii Demianenko on 06.03.2020.
+//  Copyright © 2020 Serhii Demianenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
